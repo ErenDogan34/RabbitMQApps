a1b4fda56104eb7b82c9d43e2b95e020dd0b8bf1
Erendogan1234 <eren.dogan@mobitek.com>
2023-04-25 17:35:06 +0300

    Add project files.

---
+++ DenemeRabbitMQ.publisher/Program.cs
@@ -0,0 +1,164 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DenemeRabbitMQ.publisher
+{
+    public enum LogNames
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var factory = new ConnectionFactory();
+            ////rabbitmq cloud sitesindeki link
+            //factory.Uri = new Uri("amqps://aecdcjws:aWAenT6o5_Y1oUgTwmLCKMemeIM-EONi@toad.rmq.cloudamqp.com/aecdcjws ");
+
+            //using var connection=factory.CreateConnection();
+
+            //var channel = connection.CreateModel();
+            ////isim,devamlı kalmasi icin,hangi kanal uzerinden baglanti saglanacak,son subscriber kalinca silinip silinmeyecegi
+            ///kuyruk olusturma
+            //channel.QueueDeclare("Hello-Queue",true,false,false);
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    string message = $"Message{x}";
+
+            //    //turkce karakteerde sorun yasanmamasi icin
+            //    var messagebody = Encoding.UTF8.GetBytes(message);
+
+            //    channel.BasicPublish(string.Empty, "Hello-Queue", null, messagebody);
+
+            //    Console.WriteLine($"Mesaj gönderildi{message}");
+            //});
+
+            //Console.ReadLine();
+
+
+
+            //var factory = new ConnectionFactory();
+
+            //factory.Uri = new Uri("amqps://aecdcjws:aWAenT6o5_Y1oUgTwmLCKMemeIM-EONi@toad.rmq.cloudamqp.com/aecdcjws ");
+
+            //using var connection = factory.CreateConnection();
+
+            //var channel = connection.CreateModel();
+            ////durable:fiziksle kaydetme
+            //channel.ExchangeDeclare("logs-fanout",durable:true,type:ExchangeType.Fanout);
+
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    string message = $"logs{x}";
+
+            //    //turkce karakteerde sorun yasanmamasi icin
+            //    var messagebody = Encoding.UTF8.GetBytes(message);
+
+            //    channel.BasicPublish("logs-fanout", "", null, messagebody);
+
+            //    Console.WriteLine($"Mesaj gönderildi{message}");
+            //});
+
+            //Console.ReadLine();
+
+
+            //var factory = new ConnectionFactory();
+
+            //factory.Uri = new Uri("amqps://aecdcjws:aWAenT6o5_Y1oUgTwmLCKMemeIM-EONi@toad.rmq.cloudamqp.com/aecdcjws ");
+
+            //using var connection = factory.CreateConnection();
+
+            //var channel = connection.CreateModel();
+            ////durable:fiziksle kaydetme
+            //channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+            //Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            //{
+            //    var routeKey = $"route-{x}";
+            //    var queueName = $"direct-queeu-{x}";
+            //    channel.QueueDeclare(queueName,true,false,false);
+            //    channel.QueueBind(queueName,"logs-direct",routeKey,null);
+            //});
+
+
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    LogNames log = (LogNames) new Random().Next(1,5);
+            //    string message = $"log-type{log}";
+
+            //    //turkce karakteerde sorun yasanmamasi icin
+            //    var messagebody = Encoding.UTF8.GetBytes(message);
+            //    var routeKey = $"route-{log}";
+            //    channel.BasicPublish("logs-direct", routeKey, null, messagebody);
+
+            //    Console.WriteLine($"Log gönderildi{message}");
+            //});
+
+            //Console.ReadLine();
+
+
+
+            //var factory = new ConnectionFactory();
+
+            //factory.Uri = new Uri("amqps://aecdcjws:aWAenT6o5_Y1oUgTwmLCKMemeIM-EONi@toad.rmq.cloudamqp.com/aecdcjws ");
+
+            //using var connection = factory.CreateConnection();
+
+            //var channel = connection.CreateModel();
+            ////durable:fiziksle kaydetme
+            //channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
+            //Random rnd = new Random();
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{ 
+            //    LogNames log = (LogNames)new Random().Next(1, 5);
+
+            //    LogNames log1 = (LogNames)rnd.Next(1, 5);
+            //    LogNames log2 = (LogNames)rnd.Next(1, 5);
+            //    LogNames log3 = (LogNames)rnd.Next(1, 5);
+            //    var routeKey = $"{log1}.{log2}.{log3}";
+
+            //    string message = $"log-type{log1}-{log2}-{log3}";
+            //    var messagebody = Encoding.UTF8.GetBytes(message);
+
+
+            //    channel.BasicPublish("logs-topic", routeKey, null, messagebody);
+
+            //    Console.WriteLine($"Log gönderildi{message}");
+            //});
+
+            //Console.ReadLine();
+
+
+
+            var factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqps://aecdcjws:aWAenT6o5_Y1oUgTwmLCKMemeIM-EONi@toad.rmq.cloudamqp.com/aecdcjws ");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            //durable:fiziksle kaydetme
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+           
+            Dictionary<string,object> headers = new Dictionary<string, object>();
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+            var properties = channel.CreateBasicProperties();
+            properties.Headers=headers;
+            //kalici hale getirir.
+            properties.Persistent = true;
+            var mesaj = Encoding.UTF8.GetBytes("Header mesajım");
+            channel.BasicPublish("header-exchange",string.Empty,properties,mesaj);
+            Console.WriteLine("Header olustu");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ DenemeRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,191 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace DenemeRabbitMQ.subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var factory = new ConnectionFactory();
+            ////rabbitmq cloud sitesindeki link
+            //factory.Uri = new Uri("amqps://aecdcjws:aWAenT6o5_Y1oUgTwmLCKMemeIM-EONi@toad.rmq.cloudamqp.com/aecdcjws ");
+
+            //using var connection = factory.CreateConnection();
+
+            //var channel = connection.CreateModel();
+
+            ////mesaj bolmeyi sagliyor. true olsaydi mesela 4 tane ise 2 2 boler. false olsaydi tek seferde iki prosesede ikinci degere yazilan kadar gonderir.
+            //channel.BasicQos(0, 1, false);
+
+            ////isim,devamlı kalmasi icin,hangi kanal uzerinden baglanti saglanacak,son subscriber kalinca silinip silinmeyecegi
+            ////channel.QueueDeclare("Hello-Queue", true, false, false);
+
+            //var consumer = new EventingBasicConsumer(channel);
+
+            //channel.BasicConsume("Hello-Queue", false, consumer);
+
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //    Thread.Sleep(1500);
+            //    Console.WriteLine("Gelen mesaj" + message);
+
+            //    channel.BasicAck(e.DeliveryTag,false);
+
+            //};
+
+            ////Console.ReadLine();
+            //var factory = new ConnectionFactory();
+            ////rabbitmq cloud sitesindeki link
+            //factory.Uri = new Uri("amqps://aecdcjws:aWAenT6o5_Y1oUgTwmLCKMemeIM-EONi@toad.rmq.cloudamqp.com/aecdcjws ");
+
+            //using var connection = factory.CreateConnection();
+
+            //var channel = connection.CreateModel();
+
+            //var randomQueueName = channel.QueueDeclare().QueueName;
+
+            ////kuyruk silinmeyeceğii icin sabit bir kuyruk olusturuldu.
+            ////var randomQueueName = "log-database-save-queue";
+            ////true,false,false=1,2,3 : 1:Fiziksel kayit yapilsin 2:baska kanallardan kuyruga baglanirsin 3:otomatik silinmesin
+            ////kutruk silinmeyecek.
+            ////channel.QueueDeclare(randomQueueName,true,false,false);
+
+
+            //channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
+
+            //channel.BasicQos(0, 1, false);
+
+
+
+            //var consumer = new EventingBasicConsumer(channel);
+
+            //channel.BasicConsume(randomQueueName, false, consumer);
+
+            //Console.WriteLine("Loglar dinleniyor");
+
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            //    Thread.Sleep(1500);
+            //    Console.WriteLine("Gelen mesaj" + message);
+
+            //    channel.BasicAck(e.DeliveryTag, false);
+
+            //};
+
+            //Console.ReadLine();
+
+            //----------------------------------------------------------------------------------------------
+            //var factory = new ConnectionFactory();
+            //factory.Uri = new Uri("amqps://aecdcjws:aWAenT6o5_Y1oUgTwmLCKMemeIM-EONi@toad.rmq.cloudamqp.com/aecdcjws ");
+
+            //using var connection = factory.CreateConnection();
+
+            //var channel = connection.CreateModel();
+
+
+            //channel.BasicQos(0, 1, false);
+            //var consumer = new EventingBasicConsumer(channel);
+
+            //var queueName = "direct-queeu-Critical";
+            //channel.BasicConsume(queueName, false, consumer);
+
+            //Console.WriteLine("Logları dinleniyor...");
+
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+            //    Thread.Sleep(1500);
+            //    Console.WriteLine("Gelen Mesaj:" + message);
+
+            //     File.AppendAllText("log-critical.txt", message+ "\n");
+
+            //    channel.BasicAck(e.DeliveryTag, false);
+            //};
+
+            //Console.ReadLine();
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+            //var factory = new ConnectionFactory();
+            //factory.Uri = new Uri("amqps://aecdcjws:aWAenT6o5_Y1oUgTwmLCKMemeIM-EONi@toad.rmq.cloudamqp.com/aecdcjws ");
+
+            //using var connection = factory.CreateConnection();
+
+            //var channel = connection.CreateModel();
+
+
+            //channel.BasicQos(0, 1, false);
+            //var consumer = new EventingBasicConsumer(channel);
+
+            //var queueName = channel.QueueDeclare().QueueName;
+            //var routeKey = "*.Error.*";
+            //channel.QueueBind(queueName, "logs-topic", routeKey);
+            //channel.BasicConsume(queueName, false, consumer);
+
+            //Console.WriteLine("Logları dinleniyor...");
+
+            //consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            //{
+            //    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+            //    Thread.Sleep(1500);
+            //    Console.WriteLine("Gelen Mesaj:" + message);
+
+            //    File.AppendAllText("log-critical.txt", message + "\n");
+
+            //    channel.BasicAck(e.DeliveryTag, false);
+            //};
+
+            //Console.ReadLine();
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://aecdcjws:aWAenT6o5_Y1oUgTwmLCKMemeIM-EONi@toad.rmq.cloudamqp.com/aecdcjws ");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            Dictionary<string,object> headers = new Dictionary<string, object>();
+
+            headers.Add("format","pdf");
+            headers.Add("shape","a4");
+            headers.Add("x-match","all");
+
+
+            channel.QueueBind(queueName, "header-exchange",string.Empty,headers);
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("Logları dinleniyor...");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen Mesaj:" + message);
+
+                File.AppendAllText("log-critical.txt", message + "\n");
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+            Console.ReadLine();
+        }
+    }
+}
